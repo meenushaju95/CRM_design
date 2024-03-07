81e30c08a123640b8050b5b4f79d8daf440e27c3
meenushaju95 <meenushaju95@gmail.com>
2024-03-07 17:17:37 +0530

    2

--- crm_app/urls.py
+++ crm_app/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 
 urlpatterns = [
     path('',views.home,name=''),
+    path('archive',views.archive,name='archive')
 ]
--- crm_app/views.py
+++ crm_app/views.py
@@ -6,3 +6,5 @@ def home(request):
     return render(request,'land_page.html')
 
 
+def archive(request):
+    return render(request,'archive.html')
