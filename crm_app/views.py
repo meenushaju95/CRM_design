5340109ff700b4e1820777ec78bdf4aa4edd34ea
meenushaju95 <meenushaju95@gmail.com>
2024-03-12 17:13:13 +0530

    6

--- crm_app/views.py
+++ crm_app/views.py
@@ -3,8 +3,15 @@ from django.shortcuts import render
 
 def home(request):
    
-    return render(request,'land_page.html')
+    restored_card = request.GET.get('restoredCard')
+    card_id = request.GET.get('cardId')
 
+   
+    context = {
+        'restored_card': restored_card,
+        'card_id': card_id,
+    }
+    return render(request, 'land_page.html', context)
 
 def archive(request):
     return render(request,'archive.html')
